24a84ecdc89acb1771d5f57e5d638b9f54d24de8
Runhe Zhong <runhe.zhong@gmail.com>
2019-09-25 17:53:43 -0400

    Created a basic server using Node.js

---
+++ server/server.js
@@ -0,0 +1,62 @@
+/* Testing: (user: user password: password)
+http://localhost:3000/index.html
+http://localhost:3000/users
+*/
+
+//Cntl+C to stop server
+
+var express = require('express');
+var exphbs  = require('express-handlebars');
+//var path = require('path');
+//var favicon = require('serve-favicon');
+var logger = require('morgan');
+
+var app = express(); //create express middleware dispatcher
+
+const PORT = process.env.PORT || 3000
+const ROOT_DIR = ['/'];
+
+// view engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars'); //use hbs handlebars wrapper
+
+app.locals.pretty = true; //to generate pretty view-source code in browser
+
+//read routes modules
+var routes = require('../routes/index');
+
+//some logger middleware functions
+function methodLogger(request, response, next) {
+    console.log("METHOD LOGGER");
+    console.log("================================");
+    console.log("METHOD: " + request.method);
+    console.log("URL:" + request.url);
+    next(); //call next middleware registered
+}
+function headerLogger(request, response, next) {
+    console.log("HEADER LOGGER:")
+    console.log("Headers:")
+    for (k in request.headers) console.log(k);
+    next(); //call next middleware registered
+}
+
+//register middleware with dispatcher
+//ORDER MATTERS HERE
+//middleware
+app.use(routes.authenticate); //authenticate user
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(methodLogger);
+//routes
+app.get(ROOT_DIR, routes.index);
+app.get('/index.html', routes.index);
+//app.get('/find', routes.find);
+app.get('/users', routes.users);
+//app.get('/musics/*', routes.musicDetails);
+app.post('/*', routes.index);
+
+//start server
+app.listen(PORT, err => {
+    if (err) console.log(err)
+    else { console.log(`Server listening on port: ${PORT} CNTL:-C to stop`) }
+})
